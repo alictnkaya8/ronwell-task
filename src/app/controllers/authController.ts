896af3b59d4753d95029c3d5e8d26dbfd9c7b1b9
Ali Cetinkaya <alictnkaya8@gmail.com>
2024-06-09 14:27:44 +0300

    FIX: package update

--- src/app/controllers/authController.ts
+++ src/app/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 import userService from "../services/userService";
